5ff7ddb913da5fcb3fd55ffe925e6d1148c258ab
pedtoledo2000 <pedro.toledo2@fatec.sp.gov.br>
2026-03-07 09:03:12 -0300

    adcionando anotações

--- aula-03/CalculadoraBasica.java
+++ aula-03/CalculadoraBasica.java
@@ -4,7 +4,7 @@ public class CalculadoraBasica {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         String comando = "";
-
+// menu somente so para mostrar para o usuario
         while (!comando.equalsIgnoreCase("fim")) {
             System.out.println("\n--- MENU DE OPERAÇÕES ---");
             System.out.println("Digite + para Somar");
@@ -16,13 +16,17 @@ public class CalculadoraBasica {
             System.out.print("Escolha uma opção: ");
             comando = leitor.nextLine();
 
-            // Verificamos se o comando é uma das operações antes de pedir os números
+            // verificar qual é o comando e oque ele quer
+        
             if (comando.equals("+") || comando.equals("-") || comando.equals("*") || comando.equals("/")) {
                 
+
                 System.out.print("Digite o primeiro número: ");
                 double num1 = Double.parseDouble(leitor.nextLine());
                 System.out.print("Digite o segundo número: ");
                 double num2 = Double.parseDouble(leitor.nextLine());
+
+                // if para saber qual equação usar 
 
                 if (comando.equals("+")) {
                     System.out.println("Resultado: " + (num1 + num2));
@@ -34,9 +38,11 @@ public class CalculadoraBasica {
                     if (num2 != 0) {
                         System.out.println("Resultado: " + (num1 / num2));
                     } else {
-                        System.out.println("Erro: Divisão por zero não permitida!");
+                        System.out.println("Erro: Divisão por zero não permitida!"); 
+                        // divisão por zero não tem jeito
                     }
                 }
+                // caso ele coloque algo que não esta esperando 
             } else if (!comando.equalsIgnoreCase("fim")) {
                 System.out.println("Comando inválido! Tente novamente.");
             }
